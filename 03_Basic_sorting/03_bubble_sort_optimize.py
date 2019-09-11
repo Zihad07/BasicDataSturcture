429856f23bd5d1d2af786d8562d5ae00ce015c16
zihad07 <zihadnayem8gmail.com>
2019-09-11 15:58:45 +0600

    working with Selection and Bubble Sort

---
+++ 03_Basic_sorting/03_bubble_sort_optimize.py
@@ -0,0 +1,44 @@
+
+def bubble_sort(L):
+
+    n = len(L)
+
+    operation_cnt = 0
+    for i in range(n):
+        
+        # for efficient way
+        flag_sort = True
+
+        for j in range(0, n-i-1):
+        # for j in range(0, n-1):
+            operation_cnt += 1 
+
+            if L[j] > L[j+1]:
+                flag_sort = False
+                # swap
+                L[j], L[j+1] = L[j+1], L[j]
+
+
+        if flag_sort:
+            break
+        
+        print('step-',i+1,':',L)
+    
+    return operation_cnt
+
+
+
+# ----------------------------------------------------------
+
+if __name__ == '__main__':
+
+    L = [6,1,4,9,2]
+    # L = [6,1,4,9,2,8,0,9,9,12,-1,4]
+
+    print('Befor sort:', L)
+
+    noof_operation = bubble_sort(L)
+    print('Number of operation:',noof_operation)
+    
+
+    print('After sort:',L)
